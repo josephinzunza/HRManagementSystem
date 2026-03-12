d6cc9e0b82ebbe54a844301df03cf3225738d184
Humberto Inzunza <josehinzunza@hotmail.com>
2026-03-12 14:12:00 -0700

    Added DataGrid to EmployeesView and created EmployeesViewModel.

---
+++ HRManagementSystem/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace HRManagementSystem.ViewModels
+{
+    class EmployeeViewModel
+    {
+        public Image ProfilePicture { get; set; } = new();
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+    }
+
+    class EmployeesViewModel
+    {
+        public ObservableCollection<EmployeeViewModel> Employees { get; set; } = [];
+    }
+}
